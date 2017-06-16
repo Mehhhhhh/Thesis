5f00fff0b8c55cda98f9199d1c79c76edfc2dc0f
Mehhhhhh <bruyereseb@gmail.com>
2017-06-16 23:45:32 +0100

    light flashing

---
+++ Prac/Assets/Scripts/randomlight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomlight : MonoBehaviour {
+
+    System.Random rand;
+
+	// Use this for initialization
+	void Start () {
+        rand = new System.Random();
+        StartCoroutine(flashing());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+    private IEnumerator flashing() {
+        while (true)
+        {
+            GetComponent<Light>().enabled = true;
+            yield return new WaitForSeconds(rand.Next(2, 10));
+            GetComponent<Light>().enabled = false;
+            yield return new WaitForSeconds(rand.Next(1, 2));
+        }
+    }
+}
