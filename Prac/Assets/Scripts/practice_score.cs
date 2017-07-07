2a8365ee2c15210c0b68b37a913ef60dae2c996f
Mehhhhhh <bruyereseb@gmail.com>
2017-07-07 12:20:34 +0100

    target system added

---
+++ Prac/Assets/Scripts/practice_score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class practice_score : MonoBehaviour {
+
+    private int score;
+    public GameObject scoredisplay;
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    private void Update()
+    {
+        scoredisplay.GetComponent<Text>().text = "" + score;
+    }
+
+}
